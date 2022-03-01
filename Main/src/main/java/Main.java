e6f20be675e02ea7d821ad65b4e92380d475132f
nadavq <72075136+nadavq@users.noreply.github.com>
2022-03-01 13:53:11 +0200

    updates

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -5,18 +5,6 @@ import java.io.InputStream;
 import java.util.Map;
 import java.util.Scanner;
 
-/*
-The time complexity of the solution is O(n) while n is the number of entries that appear in the file.
-That is obtained by the fact that for every id user we extract from each line, we check
-if it already exists in the daily active user's entity set, which takes O(1).
-For d - the number of individual dates that appear in the file and for k - the number of individual users that
-entered for each date, the space complexity is - O(d+k), as every date appears once in the map
-and every user id entered at that date is contained in a set in that key.
-
-The runtime complexity cannot be improved more than that because in each case we will have to go
-through each line in the file in order to check if the current id has already entered on that date.
- */
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
